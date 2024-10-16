afdfbfc2825ad5345b36e64c1678b29621b57e42
Laura <laura_habdul@hotmail.fr>
2024-10-16 20:21:23 +0200

    Initial commit

---
+++ src/main/java/com/arago/adserver/AdserverApplication.java
@@ -0,0 +1,13 @@
+package com.arago.adserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arago/adserver/AdserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arago.adserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

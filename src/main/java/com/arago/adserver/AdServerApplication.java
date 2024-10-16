8de5c4521fece00b068d4240500a205d7f81712a
Laura <laura_habdul@hotmail.fr>
2024-10-16 20:22:20 +0200

    refactor: rename application AdServerApplication

--- src/main/java/com/arago/adserver/AdServerApplication.java
+++ src/main/java/com/arago/adserver/AdServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdserverApplication {
+public class AdServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdserverApplication.class, args);
+		SpringApplication.run(AdServerApplication.class, args);
 	}
 
 }
--- src/test/java/com/arago/adserver/AdServerApplicationTests.java
+++ src/test/java/com/arago/adserver/AdServerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AdserverApplicationTests {
+class AdServerApplicationTests {
 
 	@Test
 	void contextLoads() {

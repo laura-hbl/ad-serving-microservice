a4f9050fcedb49b1f5e4645466773a3f9950e6c2
Laura <laura_habdul@hotmail.fr>
2024-10-17 15:31:10 +0200

    feat: add post endpoint to create ad, add get endpoint to retrieve ad by id

---
+++ src/main/java/com/arago/adserver/controller/AdController.java
@@ -0,0 +1,35 @@
+package com.arago.adserver.controller;
+
+import com.arago.adserver.dto.AdDto;
+import com.arago.adserver.service.AdService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ads")
+public class AdController {
+
+    private final AdService adService;
+
+    @Autowired
+    public AdController(final AdService adService) {
+        this.adService = adService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AdDto> createAd(@RequestBody final AdDto adDto) {
+        AdDto createdAd = adService.createAd(adDto);
+
+        return new ResponseEntity<>(createdAd, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AdDto> getAd(@PathVariable("id") final String adId) {
+
+        AdDto ad = adService.getAd(adId);
+        return new ResponseEntity<>(ad, HttpStatus.OK);
+    }
+
+}

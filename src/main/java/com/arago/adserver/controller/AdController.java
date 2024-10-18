22c33a8e83389914fc1101c18c2c862c136c5c22
Laura <laura_habdul@hotmail.fr>
2024-10-18 16:10:29 +0200

    feat: add rate limiting mechanism for ad creation

--- src/main/java/com/arago/adserver/controller/AdController.java
+++ src/main/java/com/arago/adserver/controller/AdController.java
@@ -2,28 +2,45 @@ package com.arago.adserver.controller;
 
 import com.arago.adserver.dto.AdDto;
 import com.arago.adserver.service.AdService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
+
 @RestController
 @RequestMapping("/ads")
 public class AdController {
 
     private final AdService adService;
+    private final Bucket bucket;
 
     @Autowired
     public AdController(final AdService adService) {
         this.adService = adService;
+
+        //rate limit of 5 requests per minute
+        Refill refill = Refill.intervally(5, Duration.ofMinutes(1));
+        Bandwidth limit = Bandwidth.classic(5, refill);
+        this.bucket = Bucket.builder()
+                .addLimit(limit)
+                .build();
     }
 
     @PostMapping
-    public ResponseEntity<AdDto> createAd(@Valid @RequestBody final AdDto adDto) {
-        AdDto createdAd = adService.createAd(adDto);
-
-        return new ResponseEntity<>(createdAd, HttpStatus.CREATED);
+    public ResponseEntity<?> createAd(@Valid @RequestBody final AdDto adDto) {
+        if (bucket.tryConsume(1)) {
+            AdDto createdAd = adService.createAd(adDto);
+            return new ResponseEntity<>(createdAd, HttpStatus.CREATED);
+        } else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .body("Rate limit exceeded. Try again later.");
+        }
     }
 
     @GetMapping("/{id}")
